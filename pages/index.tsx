fa6edc5bbfdf186e2b1b2a4b7a6267883d934a73
David Olanipekun <olaide.olanipekun@yahoo.com>
2023-01-05 12:05:21 +0000

    fix

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,6 @@
-import Head from "next/head";
-import { Inter } from "@next/font/google";
-import styles from "../styles/Home.module.css";
+import Head from 'next/head';
+import { Inter } from '@next/font/google';
+import styles from '../styles/Home.module.css';
 
 export default function Home() {
   return (
