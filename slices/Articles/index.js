2a12a39b50a61c0cd8bb29ea13c068d552a96514
DmitriyKaplan28 <dsk007@mail.ru>
2023-02-16 18:50:52 +0300

    feat: our work page

--- slices/Articles/index.js
+++ slices/Articles/index.js
@@ -1,5 +1,7 @@
 import React from 'react'
-import { PrismicRichText } from '@prismicio/react'
+import { PrismicRichText, PrismicLink } from '@prismicio/react'
+import { RichText, Img, Box } from '../../components/base';
+import { asDate } from '@prismicio/helpers';
 
 /**
  * @typedef {import("@prismicio/client").Content.ArticlesSlice} ArticlesSlice
@@ -7,30 +9,23 @@ import { PrismicRichText } from '@prismicio/react'
  * @param { ArticlesProps }
  */
 const Articles = ({ slice }) => (
-  <section>
-    <span className="title">
-      {
-        slice.primary.title ?
-        <PrismicRichText field={slice.primary.title}/>
-        : <h2>Template slice, update me!</h2>
-      }
-    </span>
-    {
-      slice.primary.description ?
-      <PrismicRichText field={slice.primary.description}/>
-      : <p>start by editing this slice from inside Slice Machine!</p>
-    }
-    <style jsx>{`
-        section {
-          max-width: 600px;
-          margin: 4em auto;
-          text-align: center;
-        }
-        .title {
-          color: #8592e0;
-        }
-    `}</style>
-  </section>
+  <Box className='bg-[#f5f5f5] py-28'>
+    <Box className="">
+      <RichText field={slice.primary.title} className="font-bold text-[17px] text-center mb-28" />
+    </Box >
+    <Box className="flex justify-center max-w-[750px] px-3 m-auto"> {
+      slice?.items?.map((item, i) =>
+        <Box key={i} className="w-[280px] mr-12">
+          <Img src={item.article_image.url} alt={item.article_image.alt} className="w-[280px] h-[200px] mb-[5px]" />
+          <span className="text-[#f9b807] text-[11px]">{(item.article_date).toString()}</span>
+          <RichText field={item.article_title} className="text-base font-medium my-2" />
+          <RichText field={item.article_text} className="text-[11px] text-left" />
+          {/* <PrismicLink field={item.article_link}>My Link</PrismicLink> */}
+
+        </Box>
+      )
+    }</Box>
+  </Box>
 )
 
 export default Articles
--- slices/OurworkSection/index.js
+++ slices/OurworkSection/index.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { PrismicRichText, PrismicLink } from '@prismicio/react'
+import { RichText, Img, Box } from '../../components/base';
 
 /**
  * @typedef {import("@prismicio/client").Content.OurworkSectionSlice} OurworkSectionSlice
@@ -7,35 +8,24 @@ import { PrismicRichText, PrismicLink } from '@prismicio/react'
  * @param { OurworkSectionProps }
  */
 const OurworkSection = ({ slice }) => (
-  <section>
-    <span className="title">
-    
-          <PrismicRichText field={slice.primary.title} />
-          
-    </span>
-    {
-      slice?.items?.map((item, i) =>
-      <div>
+  <Box className='flex flex-col items-center'>
+    <Box className="box-border pt-16 h-full w-full flex justify-center items-center bg-static-bg min-h-[568px] bg-no-repeat bg-center bg-cover">
+      <RichText className="text-white font-bold text-2xl mb-8" field={slice.primary.title} />
+    </Box>
+    <Box className='w-full bg-white flex justify-center'>
+      <Box className='flex justify-start flex-wrap px-[15px] py-[100px] bg-white max-w-[990px]'>{
+        slice?.items?.map((item, i) =>
+          <Box className='w-[217px] m-auto my-4' key={i}>
+            <Img src={item.project_image.url} alt={item.project_image.alt} className="w-[217px] h-[287px]" />
+            <RichText field={item.project_title} className="text-left text-[28px] text-black mt-3 box-border block" />
+            <RichText field={item.project_description} className="text-[11px]" />
+            <PrismicLink field={item.project_link} className="text-[#f9b807] text-[11px] font-bold">Coming soon</PrismicLink>
+          </Box>
+        )
+      }</Box>
 
-        <img src={item.project_image.url} alt={item.project_image.alt} />
-        <PrismicRichText field={item.project_title} />
-        <PrismicRichText field={item.project_description} />        
-        <PrismicLink field={item.project_link}>My Link</PrismicLink>
-      </div>
-      )
-    }
-
-    <style jsx>{`
-        section {
-          max-width: 600px;
-          margin: 4em auto;
-          text-align: center;
-        }
-        .title {
-          color: #8592e0;
-        }
-    `}</style>
-  </section>
+    </Box>
+  </Box>
 )
 
 export default OurworkSection
